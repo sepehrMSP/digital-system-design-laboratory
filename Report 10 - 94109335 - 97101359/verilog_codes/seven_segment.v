3ecef865ac6c83669882606b0a8aab1996017824
sepehrMSP <sepehrpourghannad@yahoo.com>
2020-08-13 00:13:18 +0430

    Add stack base cpu

---
+++ Report 10 - 94109335 - 97101359/verilog_codes/seven_segment.v
@@ -0,0 +1,26 @@
+module seven_segment(bcd, seg);
+    
+    input [3:0] bcd;
+    output reg [6:0] seg;
+
+    //=====================================
+
+    always @(bcd)
+    begin
+        case (bcd)
+            0 : seg = 7'b0000001;
+            1 : seg = 7'b1001111;
+            2 : seg = 7'b0010010;
+            3 : seg = 7'b0000110;
+            4 : seg = 7'b1001100;
+            5 : seg = 7'b0100100;
+            6 : seg = 7'b0100000;
+            7 : seg = 7'b0001111;
+            8 : seg = 7'b0000000;
+            9 : seg = 7'b0000100;
+            //switch off 7 segment character when the bcd digit is not a decimal number.
+            default : seg = 7'b1111111; 
+        endcase
+    end
+    
+endmodule
